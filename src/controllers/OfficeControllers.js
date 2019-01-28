95be90710a8f837de911b2f609be89931e820f98
Tijani Femi <40898511+femitj@users.noreply.github.com>
2019-01-28 08:53:23 +0100

    feat(endpoint): adds api endpoint that gets ALL political offices [delivers #163500485] (#21)

--- src/controllers/OfficeControllers.js
+++ src/controllers/OfficeControllers.js
@@ -2,6 +2,15 @@ import db from '../models/politicaloffice';
 
 class OfficeController {
 
+  // get all political parties
+  static getAllOffices(req, res) {
+    return res.status(200).json({
+      status: 200,
+      message: 'All political office record successfully retrieved',
+      data: db,
+       });
+    }
+
   // get a single political office
   static getOffice(req, res) {
     const office = db.find(c => c.id === parseInt((req.params.id), 10));
