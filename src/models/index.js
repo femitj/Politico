81889deaa75a1d1d79d1e685a2ce35c4ef5121e3
Tijani Oluwafemi <olutimedia@gmail.com>
2019-01-28 18:30:51 +0100

    setting up PostgresSQL database [finishes #163514554]

--- src/models/index.js
+++ src/models/index.js
@@ -9,21 +9,15 @@ const pool = new Pool({
 });
 
 export default {
-  /**
-   * DB Query
-   * @param {object} req
-   * @param {object} res
-   * @returns {object} object 
-   */
   query(text, params){
     return new Promise((resolve, reject) => {
       pool.query(text, params)
-      .then((res) => {
-        resolve(res);
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          reject(err);
+        })
       })
-      .catch((err) => {
-        reject(err);
-      })
-    })
+    }
   }
-}
