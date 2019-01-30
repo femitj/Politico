5fc4b1596136e614f3f14bed37f8d08d8cf1cdbb
Tijani Femi <40898511+femitj@users.noreply.github.com>
2019-01-30 12:35:41 +0100

    #163570814 Integrate office routes with POSTGRES database and authentication (#29)
    * feat(ui): Integrate POSTGRES database with office routes and authentication [delivers #163570814]
    
    * updated response spec

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userController from '../controllers/User';
+import OfficeController from '../controllers/OfficeControllers';
 import PartyController from '../controllers/PartyControllers';
 import Auth from '../middleware/Auth';
 import checkSignUpInput from '../helpers/checkSignUpInput';
@@ -8,12 +9,18 @@ import checkSignInInput from '../helpers/signInInput';
 // route handler
 const router = express.Router();
 
+// Political office routes
+router.post('/api/v1/offices', Auth.verifyToken, OfficeController.createOffice);
+router.get('/api/v1/offices', Auth.verifyToken, OfficeController.getAllOffices);
+router.get('/api/v1/offices/:id', Auth.verifyToken, OfficeController.getOffice);
+
 // Political party routes
 router.post('/api/v1/parties', Auth.verifyToken, PartyController.createParty);
 router.get('/api/v1/parties', Auth.verifyToken, PartyController.getAllParties);
 router.get('/api/v1/parties/:id', Auth.verifyToken, PartyController.getParty);
 router.patch('/api/v1/parties/:id', Auth.verifyToken, PartyController.updatePartyName);
 router.delete('/api/v1/parties/:id', Auth.verifyToken, PartyController.deleteParty);
+
 
 // Users routes
 router.post('/api/v1/users', checkSignUpInput, userController.create);
