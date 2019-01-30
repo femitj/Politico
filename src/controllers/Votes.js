13a91e9b33f1058e99f62494ec3df50065a700db
Tijani Femi <40898511+femitj@users.noreply.github.com>
2019-01-30 12:42:10 +0100

    #163530828 Users can vote for candidate of choice   (#31)
    * feat(endpoint): Users can vote for candidate of choice [delivers #163530828]
    
    * updated response spec
    
    * updated response spec

---
+++ src/controllers/Votes.js
@@ -0,0 +1,37 @@
+import db from '../models/index';
+
+
+class vote {
+  static async create(req, res) {
+    const createQuery = `INSERT INTO
+    votes(office, candidate, voter, createdOn)
+    VALUES($1, $2, $3, $4)
+    returning *`;
+
+    const values = [
+      req.body.office,
+      req.body.candidate,
+      req.user.id,
+      req.body.createdOn,
+    ];
+
+    try {
+      const { rows } = await db.query(createQuery, values);
+      return res.status(201).send({
+        status: 201,
+        message: 'vote created',
+        data: [{
+          id: rows[0].id,
+          office: rows[0].office,
+          candidate: rows[0].candidate,
+          voter: rows[0].voter,
+        }],
+      });
+    } 
+    catch (error) {
+      return res.status(400).send(error);
+    }
+  }
+}
+
+export default vote;
