96bf07b7427c23219823f99f586ec76464559d4a
Tijani Oluwafemi <olutimedia@gmail.com>
2019-01-30 06:53:54 +0100

    feat(ui): Integrate POSTGRES database with office routes and authentication [delivers #163570814]

---
+++ src/controllers/OfficeControllers.js
@@ -0,0 +1,79 @@
+import db from '../models/index';
+import Helper from '../helpers/Helper';
+
+class OfficeController {
+  static async createOffice(req, res) {
+    const createQuery = `INSERT INTO
+    offices(type, name, createdBy, createdOn)
+    VALUES($1, $2, $3, $4)
+    returning *`;
+
+    const values = [
+      req.body.type,
+      req.body.name,
+      req.user.id,
+      req.body.createdOn,
+    ];
+ 
+    try {
+      const { rows } = await db.query(createQuery, values);
+      return res.status(201).send({
+        status: 201,
+        data: [{
+          id: rows[0].id,
+          message: 'Political office created',
+        }],
+      });
+    } 
+    catch (error) {
+      console.log(error);
+      return res.status(400).send(error);
+    }
+  }
+
+  // get all political office
+  static async getAllOffices(req, res) {
+    const getAllOffices = 'SELECT * FROM offices where createdBy = $1';
+    try {
+      const { rows, rowCount } = await db.query(getAllOffices, [req.user.id]);
+      return res.status(200).send({
+        status: 200,
+        message: 'All political office record successfully retrieved',
+        data: [{
+          data: rows,
+        }],
+      });
+    } 
+    catch (error) {
+      return res.status(400).send(error);
+    }
+  }
+
+  // get a single political office
+  static async getOffice(req, res) {
+    const text = 'SELECT * FROM offices WHERE office_id = $1 AND createdBy = $2';
+    try {
+      const { rows } = await db.query(text, [req.params.id, req.user.id]);
+      if (!rows[0]) {
+        return res.status(404).json({
+          status: 404,
+          error: 'Political office not found',
+        });
+      }
+      return res.status(200).send({
+        status: 200,
+        message: '/id exists, Political office found',
+        data: [{
+          id: rows[0].id,
+          type: rows[0].type,
+          name: rows[0].name,
+        }],
+      });
+    } 
+    catch (error) {
+      console.log(error);
+      return res.status(400).send(error)
+    }
+  }
+}
+export default OfficeController;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import userController from '../controllers/User';
+import OfficeController from '../controllers/OfficeControllers';
+import Auth from '../middleware/Auth';
 import checkSignUpInput from '../helpers/checkSignUpInput';
 import checkSignInInput from '../helpers/signInInput';
 
-
-
-
 // route handler
 const router = express.Router();
+
+// Political office routes
+router.post('/api/v1/offices', Auth.verifyToken, OfficeController.createOffice);
+router.get('/api/v1/offices', Auth.verifyToken, OfficeController.getAllOffices);
+router.get('/api/v1/offices/:id', Auth.verifyToken, OfficeController.getOffice);
 
 // Users routes
 router.post('/api/v1/users', checkSignUpInput, userController.create);
