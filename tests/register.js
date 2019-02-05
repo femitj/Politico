2cafe404aeb46eb152b280a9c41ca4f2f792558b
Tijani Oluwafemi <olutimedia@gmail.com>
2019-02-05 10:43:00 +0100

    ch(feedback) LFA feedback - refactoring [finishes #163695554]

--- tests/register.js
+++ tests/register.js
@@ -30,14 +30,14 @@ describe('POST api/v1/auth/login', () => {
         if (err) done();
         const { body } = res;
         // eslint-disable-next-line prefer-destructuring
-        admintoken = body.data[0].token;
+        admintoken = body.data.token;
         expect(body).to.be.an('object');
         expect(body.status).to.be.a('number');
         expect(body.status).to.be.equals(200);
-        expect(body.data[0]).to.haveOwnProperty('token');
-        expect(body.data[0]).to.haveOwnProperty('user');
-        expect(body.data[0].user).to.be.an('object');
-        expect(body.data[0].token).to.be.a('string');
+        expect(body.data).to.haveOwnProperty('token');
+        expect(body.data).to.haveOwnProperty('user');
+        expect(body.data.user).to.be.an('object');
+        expect(body.data.token).to.be.a('string');
         done();
       });
   });
@@ -60,9 +60,9 @@ describe('POST api/v1/', () => {
         const { body } = res;
         expect(body).to.be.an('object');
         expect(body.status).to.be.equals(201);
-        expect(body.data[0]).to.be.an('object');
+        expect(body.data).to.be.an('object');
         expect(body.message).to.be.a('string');
-        expect(body.data[0]).to.haveOwnProperty('candidate' && 'office');
+        expect(body.data).to.haveOwnProperty('candidate' && 'office');
         done();
       });
   });
